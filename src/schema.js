caa488696c3a339539a08f29b94e2254712c5ff6
Cristian Viorel Ceamatu <cristian.ceamatu@gmail.com>
2022-11-09 00:19:48 +0200

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -1,17 +1,8 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  type Query {
-    "Query to get tracks array for the homepage grid"
-    tracksForHome: [Track!]!
-    "Fetch a specific track, provided a track's ID"
-    track(id: ID!): Track!
-    "Fetch a specific module, provided a module's ID"
-    module(id: ID!): Module!
-  }
-
   type Mutation {
-    "Increment the number of views of a given track, when the track card is clicked"
+    "Increment views of a track"
     incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
   }
 
@@ -26,48 +17,57 @@ const typeDefs = gql`
     track: Track
   }
 
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
+    "Fetch a specific module, provided a module's ID"
+    module(id: ID!): Module
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
     "The track's title"
     title: String!
-    "The track's main Author"
+    "The track's main author"
     author: Author!
-    "The track's illustration to display in track card or track page detail"
+    "The track's main illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
     modulesCount: Int
-    "The track's complete description, can be in markdown format"
+    "The track's complete description, can be in Markdown format"
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
     "The track's complete array of Modules"
     modules: [Module!]!
+    durationInSeconds: Int
   }
 
-  "Author of a complete Track or a Module"
+  "Author of a complete Track"
   type Author {
     id: ID!
     "Author's first and last name"
     name: String!
-    "Author's profile picture"
+    "Author's profile picture url"
     photo: String
   }
 
   "A Module is a single unit of teaching. Multiple Modules compose a Track"
   type Module {
     id: ID!
-    "The module's title"
+    "The Module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
-    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    videoUrl: String!
     content: String
-    "The module's video url, for video-based modules"
-    videoUrl: String
+    "The Module's length in minutes"
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    durationInSeconds: Int
   }
 `;
 
-module.exports = typeDefs;
+module.exports = { typeDefs };
